269fb3a9487537e685f5ae57448b5b293fb97076
PhucLe0809 <lhphuc.7.01j@gmail.com>
2025-02-21 12:39:34 +0700

    Add Authorize for Feign Client

--- com-landingis-api/src/main/java/com/landingis/api/controller/LecturerSchedulerController.java
+++ com-landingis-api/src/main/java/com/landingis/api/controller/LecturerSchedulerController.java
@@ -14,6 +14,7 @@ import com.landingis.api.util.PageableUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -33,6 +34,7 @@ public class LecturerSchedulerController {
 
 
     @GetMapping("/list")
+    @PreAuthorize("hasAuthority('C_GET')")
     public ResponseEntity<ApiMessageDto<PaginationDto<LecturerSchedulerDto>>> getAll(
             LecturerSchedulerCriteria lecturerSchedulerCriteria,
             Pageable pageable
@@ -41,11 +43,13 @@ public class LecturerSchedulerController {
     }
 
     @GetMapping("/get/{id}")
+    @PreAuthorize("hasAuthority('C_GET')")
     public ResponseEntity<ApiMessageDto<LecturerSchedulerDto>> getById(@PathVariable Long id) {
         return ResponseEntity.ok(lecturerSchedulerClient.getById(id));
     }
 
     @PostMapping("/create")
+    @PreAuthorize("hasAuthority('C_POST')")
     public ResponseEntity<ApiMessageDto<LecturerSchedulerDto>> createLecturerScheduler(
             @Valid @RequestBody LecturerSchedulerCreateForm form
     ) {
@@ -59,6 +63,7 @@ public class LecturerSchedulerController {
     }
 
     @PutMapping("/update")
+    @PreAuthorize("hasAuthority('C_PUT')")
     public ResponseEntity<ApiMessageDto<LecturerSchedulerDto>> updateLecturerScheduler(
             @Valid @RequestBody LecturerSchedulerUpdateForm form
     ) {
@@ -75,6 +80,7 @@ public class LecturerSchedulerController {
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('C_DELETE')")
     public ResponseEntity<ApiMessageDto<Void>> deleteLecturerScheduler(@PathVariable Long id) {
         return ResponseEntity.ok(lecturerSchedulerClient.deleteLecturerScheduler(id));
     }
--- com-landingis-api/src/main/java/com/landingis/api/controller/PeriodController.java
+++ com-landingis-api/src/main/java/com/landingis/api/controller/PeriodController.java
@@ -10,6 +10,7 @@ import com.landingis.api.util.PageableUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.landingis.api.dto.ApiMessageDto;
 
@@ -23,6 +24,7 @@ public class PeriodController {
     private PeriodClient periodClient;
 
     @GetMapping("/list")
+    @PreAuthorize("hasAuthority('C_GET')")
     public ResponseEntity<ApiMessageDto<PaginationDto<PeriodDto>>> getPeriodsFromExtension(
             PeriodCriteria periodCriteria,
             Pageable pageable
@@ -31,21 +33,25 @@ public class PeriodController {
     }
 
     @GetMapping("/get/{id}")
+    @PreAuthorize("hasAuthority('C_GET')")
     public ResponseEntity<ApiMessageDto<PeriodDto>> getById(@PathVariable Long id) {
         return ResponseEntity.ok(periodClient.getById(id));
     }
 
     @PostMapping("/create")
+    @PreAuthorize("hasAuthority('C_POST')")
     public ResponseEntity<ApiMessageDto<PeriodDto>> createPeriod(@Valid @RequestBody PeriodCreateForm form) {
         return ResponseEntity.ok(periodClient.createPeriod(form));
     }
 
     @PutMapping("/update")
+    @PreAuthorize("hasAuthority('C_PUT')")
     public ResponseEntity<ApiMessageDto<PeriodDto>> updatePeriod(@Valid @RequestBody PeriodUpdateForm form) {
         return ResponseEntity.ok(periodClient.updatePeriod(form));
     }
 
     @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('C_DELETE')")
     public ResponseEntity<ApiMessageDto<Void>> deletePeriod(@PathVariable Long id) {
         return ResponseEntity.ok(periodClient.deletePeriod(id));
     }
